31b19973ccb4ac9b100df49ec71e48d5328eade9
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2022-12-27 17:17:15 -0300

    resetando css

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,4 +6,21 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 1px ${({ theme }) => theme.color.brand['yellow--light']};
+  }
+
+  body {
+    background-color: ${({ theme }) => theme.color.base.background};
+    color: ${({ theme }) => theme.color.base.text};
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-size: 1rem;
+    font-weight: 400;
+    -webkit-font-smoothing: antialiased;
+  }
 `
