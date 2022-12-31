0520b75dae1509b63a39ddd282bf7cf3cda58e8c
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2022-12-31 17:20:01 -0300

    finalizando componente InputQuantity

--- src/components/InputQuantity/index.tsx
+++ src/components/InputQuantity/index.tsx
@@ -1,14 +1,29 @@
 import { Minus, Plus } from 'phosphor-react'
+import { useState } from 'react'
 import { InputQuantityContainer } from './styles'
 
 export function InputQuanity() {
+  const [quantity, setQuantity] = useState(1)
+
+  function handleAddQuantity() {
+    if (quantity < 26) {
+      setQuantity(quantity + 1)
+    }
+  }
+
+  function handleRemoveQuantity() {
+    if (quantity > 1) {
+      setQuantity(quantity - 1)
+    }
+  }
+
   return (
     <InputQuantityContainer>
-      <button>
+      <button onClick={handleRemoveQuantity}>
         <Minus size={14} weight="fill" />
       </button>
-      <input type="text" value="1" disabled />
-      <button>
+      <input type="text" value={quantity} disabled />
+      <button onClick={handleAddQuantity}>
         <Plus size={14} weight="fill" />
       </button>
     </InputQuantityContainer>
