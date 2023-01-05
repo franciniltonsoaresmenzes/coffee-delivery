6d5090defdaa279c5f384e8f02b3d2cb24be8510
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-01-05 01:03:03 -0300

    Criando funções CoffeeShopProvider

--- src/contexts/CoffeeShopProvider.tsx
+++ src/contexts/CoffeeShopProvider.tsx
@@ -1,6 +1,12 @@
-import { createContext, ReactNode } from 'react'
+import { createContext, ReactNode, useReducer } from 'react'
+import { CoffeeContext, coffeeReducer } from '../reduces/coffee/reduce'
+import { addNewCoffeeShop, removeCoffeeShop } from '../reduces/coffee/actions'
 
-interface CoffeeShopContextTypes {}
+interface CoffeeShopContextTypes {
+  coffee: CoffeeContext[]
+  addCoffeeShopCart: (data: CoffeeContext) => void
+  removeCoffeShopCart: (idCoffee: string) => void
+}
 
 export const CoffeeShopContext = createContext({} as CoffeeShopContextTypes)
 
@@ -9,8 +15,22 @@ interface CoffeeShopContextProps {
 }
 
 export function CoffeeShopProvider({ children }: CoffeeShopContextProps) {
+  const [coffeeState, dispatch] = useReducer(coffeeReducer, { coffee: [] })
+
+  function addCoffeeShopCart(data: CoffeeContext) {
+    dispatch(addNewCoffeeShop(data))
+  }
+
+  function removeCoffeShopCart(idCoffee: string) {
+    dispatch(removeCoffeeShop(idCoffee))
+  }
+
+  const { coffee } = coffeeState
+
   return (
-    <CoffeeShopContext.Provider value={{}}>
+    <CoffeeShopContext.Provider
+      value={{ addCoffeeShopCart, coffee, removeCoffeShopCart }}
+    >
       {children}
     </CoffeeShopContext.Provider>
   )
